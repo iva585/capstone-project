154357c343a1d2de6d6af2bacce046b2f96cdb62
Iva Pranjic <pranjic.iva213@gmail.com>
2021-11-24 18:56:55 +0100

    feature: display shopping list

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,11 +2,19 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ShoppingList from './pages/ShoppingList/ShoppingList';
 
+// @TODO - replace with actual storage layer
+const mockItems = [
+  { title: 'milk' },
+  { title: 'oatmeal' },
+  { title: 'chocolate' },
+  { title: 'bananas' },
+];
+
 function App(): JSX.Element {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<ShoppingList />} />
+        <Route path="/" element={<ShoppingList items={mockItems} />} />
       </Routes>
     </BrowserRouter>
   );
--- src/app/pages/ShoppingList/ShoppingList.tsx
+++ src/app/pages/ShoppingList/ShoppingList.tsx
@@ -1,29 +1,21 @@
-import { Typography } from '@mui/material';
+import { Checkbox, List, ListItem, Typography } from '@mui/material';
 import React from 'react';
-import Paper from '@mui/material/Paper';
-import InputBase from '@mui/material/InputBase';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import Add from '@mui/icons-material/Add';
 
-export default function ShoppingList(): JSX.Element {
-  return (
-    <>
-      <Typography>Shopping List</Typography>
-      <Paper
-        component="form"
-        sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
-      >
-        <InputBase
-          sx={{ ml: 1, flex: 1 }}
-          placeholder="Add Item To Shopping List"
-          inputProps={{ 'aria-label': 'add item to shopping list' }}
-        />
-        <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-        <IconButton type="submit" sx={{ p: '10px' }} aria-label="add">
-          <Add />
-        </IconButton>
-      </Paper>
-    </>
-  );
-}
+type Props = {
+  items: { title: string }[];
+};
+
+export default (props: Props): JSX.Element => (
+  <>
+    <Typography>Shopping List</Typography>
+    <List>
+      {props.items.map((item) => (
+        <ListItem>
+          {' '}
+          <Checkbox edge="start" />
+          {item.title}
+        </ListItem>
+      ))}
+    </List>
+  </>
+);
