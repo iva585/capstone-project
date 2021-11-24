9dc6d6cec48f8a5f8592a5a52254de5fca3e981e
Iva Pranjic <pranjic.iva213@gmail.com>
2021-11-24 21:05:54 +0100

    feature: move crossed off items to the  end of list

--- src/app/pages/ShoppingList/ShoppingList.tsx
+++ src/app/pages/ShoppingList/ShoppingList.tsx
@@ -7,13 +7,17 @@ type Props = {
   items: ShoppingListItemType[];
 };
 
-export default (props: Props): JSX.Element => (
-  <>
-    <Typography>Shopping List</Typography>
-    <List>
-      {props.items.map((item) => (
-        <ShoppingListItem key={item.id} item={item} />
-      ))}
-    </List>
-  </>
-);
+export default (props: Props): JSX.Element => {
+  return (
+    <>
+      <Typography>Shopping List</Typography>
+      <List>
+        {props.items
+          .sort((a, b) => (a.checked ? 1 : 0) - (b.checked ? 1 : 0))
+          .map((item) => (
+            <ShoppingListItem key={item.id} item={item} />
+          ))}
+      </List>
+    </>
+  );
+};
