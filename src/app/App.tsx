0f1255e0b74da1764309bd1c1afb3fa4bd786e3f
Iva Pranjic <pranjic.iva213@gmail.com>
2021-11-24 17:39:39 +0100

    feature: add form to page shopping list

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,12 @@
-import { Typography } from '@mui/material';
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ShoppingList from './pages/ShoppingList/ShoppingList';
 
 function App(): JSX.Element {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Typography>Hello World</Typography>} />
+        <Route path="/" element={<ShoppingList />} />
       </Routes>
     </BrowserRouter>
   );
