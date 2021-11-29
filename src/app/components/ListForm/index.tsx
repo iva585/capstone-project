7c3e85cc789746e435d3f51307191fa6cedca422
Iva Pranjic <pranjic.iva213@gmail.com>
2021-11-29 11:33:06 +0100

    style: fit form  component to mobile device

--- src/app/components/ListForm/index.tsx
+++ src/app/components/ListForm/index.tsx
@@ -9,7 +9,14 @@ export default (): JSX.Element => {
   return (
     <Paper
       component="form"
-      sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
+      sx={{
+        p: '2px 4px',
+        display: 'flex',
+        alignItems: 'center',
+        maxWidth: 320,
+        alignSelf: 'center',
+        m: 1,
+      }}
     >
       <InputBase
         sx={{ ml: 1, flex: 1 }}
