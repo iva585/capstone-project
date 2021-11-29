fce774ff8ed67ddf958cf97ccc2bff5bd5ddcd0d
Iva Pranjic <pranjic.iva213@gmail.com>
2021-11-29 11:18:58 +0100

    feature: create story for form component

---
+++ src/app/components/ListForm/ListForm.stories.tsx
@@ -0,0 +1,9 @@
+import ListForm from '.';
+import React from 'react';
+
+export default {
+  title: 'Component/ShoppingListForm',
+  component: ListForm,
+};
+
+export const Form = (): JSX.Element => <ListForm />;
