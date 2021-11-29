590f7556f31ec4691075c56a7379dea6d7e13e75
Iva Pranjic <pranjic.iva213@gmail.com>
2021-11-30 00:10:16 +0100

    feature: create footer component

---
+++ src/app/components/Footer/index.tsx
@@ -0,0 +1,27 @@
+import { Paper, BottomNavigation, Fab } from '@mui/material';
+import React from 'react';
+import './index.css';
+
+type Props = {
+  actionButtonIcon: JSX.Element;
+  onClickActionButton: () => void;
+};
+
+export default (props: Props): JSX.Element => {
+  return (
+    <>
+      <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }}>
+        <BottomNavigation>
+          <Fab
+            onClick={props.onClickActionButton}
+            color="primary"
+            size="medium"
+            sx={{ mt: -3 }}
+          >
+            {props.actionButtonIcon}
+          </Fab>
+        </BottomNavigation>
+      </Paper>
+    </>
+  );
+};
--- src/app/pages/ShoppingList/ShoppingList.tsx
+++ src/app/pages/ShoppingList/ShoppingList.tsx
@@ -1,12 +1,17 @@
+import React, { useState } from 'react';
 import { List, Typography, AppBar } from '@mui/material';
-import React from 'react';
 import { useSelector } from 'react-redux';
 import ListForm from '../../components/ListForm';
 import ShoppingListItem from '../../components/ShoppingListItem';
+import Footer from '../../components/Footer';
 import type { RootState } from '../../store';
 import './index.css';
+import Close from '@mui/icons-material/Close';
+import Add from '@mui/icons-material/Add';
 
 export default (): JSX.Element => {
+  const [editing, setEditing] = useState<boolean>(false);
+
   const shoppingListItems = useSelector(
     (state: RootState) => state.shoppingList
   );
@@ -31,7 +36,13 @@ export default (): JSX.Element => {
             <ShoppingListItem key={item.id} item={item} />
           ))}
       </List>
+
       <ListForm />
+
+      <Footer
+        actionButtonIcon={editing ? <Close /> : <Add />}
+        onClickActionButton={() => setEditing((editing) => !editing)}
+      />
     </>
   );
 };
