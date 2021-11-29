2ed185b87a7078e8c222fd8e9ccaeea2ce62bf99
Iva Pranjic <pranjic.iva213@gmail.com>
2021-11-29 11:11:30 +0100

    feature: create form component

---
+++ src/app/components/ShoppingListForm/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Paper from '@mui/material/Paper';
+import InputBase from '@mui/material/InputBase';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import AddIcon from '@mui/icons-material/Add';
+
+export default (): JSX.Element => {
+  return (
+    <Paper
+      component="form"
+      sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
+    >
+      <InputBase
+        sx={{ ml: 1, flex: 1 }}
+        placeholder="Add new item"
+        inputProps={{ 'aria-label': 'add new item' }}
+      />
+      <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+      <IconButton color="primary" sx={{ p: '10px' }} aria-label="add">
+        <AddIcon />
+      </IconButton>
+    </Paper>
+  );
+};
