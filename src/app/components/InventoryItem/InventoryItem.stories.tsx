7aeb0660bdf44f08e71f6316458a9578309cd7d1
Iva Pranjic <pranjic.iva213@gmail.com>
2021-12-02 22:37:25 +0100

    feature: create storybook for inventory item

---
+++ src/app/components/InventoryItem/InventoryItem.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import InventoryItem from '.';
+
+export default {
+  title: 'Component/InventoryItem',
+  component: InventoryItem,
+};
+
+export const Item = (): JSX.Element => <InventoryItem />;
