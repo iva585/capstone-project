3f35d7dcae2d60aaaf77aa7306f270ac3dd9bcd3
Iva Pranjic <pranjic.iva213@gmail.com>
2021-12-02 22:21:26 +0100

    feature: create route for inventory page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ShoppingList from './pages/ShoppingList/ShoppingList';
+import Inventory from './pages/Inventory/Inventory';
 
 export default (): JSX.Element => (
   <BrowserRouter>
     <Routes>
       <Route path="/shopping-list" element={<ShoppingList />} />
+    </Routes>
+    <Routes>
+      <Route path="/inventory" element={<Inventory />} />
     </Routes>
   </BrowserRouter>
 );
---
+++ src/app/pages/Inventory/Inventory.tsx
@@ -0,0 +1,6 @@
+import { Typography } from '@mui/material';
+import React from 'react';
+
+export default (): JSX.Element => {
+  return <Typography>Inventory</Typography>;
+};
