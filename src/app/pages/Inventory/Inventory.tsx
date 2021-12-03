d89620cea9324468a96ca7fad19fd9f5ce163372
Iva Pranjic <pranjic.iva213@gmail.com>
2021-12-03 11:53:55 +0100

    feature: add header to inventory page

--- src/app/pages/Inventory/Inventory.tsx
+++ src/app/pages/Inventory/Inventory.tsx
@@ -1,6 +1,7 @@
 import { List, Typography } from '@mui/material';
 import React from 'react';
 import { useSelector } from 'react-redux';
+import Header from '../../components/Header';
 import InventoryItem from '../../components/InventoryItem';
 import type { RootState } from '../../store';
 
@@ -8,7 +9,7 @@ export default (): JSX.Element => {
   const inventoryItems = useSelector((state: RootState) => state.inventory);
   return (
     <>
-      <Typography>Inventory</Typography>
+      <Header children="Inventory" />
       <List sx={{ pt: 7 }}>
         {[...inventoryItems]
           .sort((a, b) => (a.checked ? 1 : 0) - (b.checked ? 1 : 0))
