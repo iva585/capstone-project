bb8a453bf10fc2664cb7dcee9086a26701878846
Iva Pranjic <pranjic.iva213@gmail.com>
2021-12-06 09:46:14 +0100

    feature: add navigation icons to footer

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { Paper, BottomNavigation, Fab } from '@mui/material';
+import { Paper, BottomNavigation, Fab, IconButton } from '@mui/material';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import KitchenIcon from '@mui/icons-material/Kitchen';
+import { useNavigate } from 'react-router';
 
 type Props = {
   actionButtonIcon: JSX.Element;
@@ -7,12 +10,16 @@ type Props = {
 };
 
 export default (props: Props): JSX.Element => {
+  const navigate = useNavigate();
   return (
     <Paper
       sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }}
       elevation={4}
     >
       <BottomNavigation>
+        <IconButton onClick={() => navigate('/inventory')} sx={{ mx: 5 }}>
+          <KitchenIcon />
+        </IconButton>
         <Fab
           onClick={props.onClickActionButton}
           color="primary"
@@ -21,6 +28,9 @@ export default (props: Props): JSX.Element => {
         >
           {props.actionButtonIcon}
         </Fab>
+        <IconButton onClick={() => navigate('/shopping-list')} sx={{ mx: 5 }}>
+          <ShoppingCartIcon />
+        </IconButton>
       </BottomNavigation>
     </Paper>
   );
