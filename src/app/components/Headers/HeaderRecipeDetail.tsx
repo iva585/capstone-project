08b9b7de3ce2b78b1f4f1f383ad8a0b29ff98247
Iva Pranjic <pranjic.iva213@gmail.com>
2021-12-07 16:25:08 +0100

    feature: add title card  to img header

--- src/app/components/Headers/HeaderRecipeDetail.tsx
+++ src/app/components/Headers/HeaderRecipeDetail.tsx
@@ -1,14 +1,51 @@
+import { Card, CardContent, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import React from 'react';
 import './index.css';
 
 export default (): JSX.Element => {
   return (
-    <Box sx={{ width: '100%', height: '200px', overflow: 'hidden' }}>
-      <img
-        className="image-header"
-        src="https://images.unsplash.com/photo-1616299908398-9af1134ad522?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=737&q=80"
-      />
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+      }}
+    >
+      <Box sx={{ width: '100%', height: '200px', overflow: 'hidden' }}>
+        <img
+          className="image-header"
+          src="https://images.unsplash.com/photo-1616299908398-9af1134ad522?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=737&q=80"
+          alt="pesto pasta"
+        />
+      </Box>
+      <Card
+        sx={{
+          mt: -7,
+          alignSelf: 'center',
+          width: '70%',
+          p: 0,
+          borderRadius: '20px',
+        }}
+      >
+        <CardContent
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Typography gutterBottom variant="h5">
+            Pesto Pasta
+          </Typography>
+          <Typography
+            variant="body2"
+            color="text.secondary"
+            sx={{ textAlign: 'center' }}
+          >
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit!
+          </Typography>
+        </CardContent>
+      </Card>
     </Box>
   );
 };
