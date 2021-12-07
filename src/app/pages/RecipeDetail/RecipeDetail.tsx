c0cd3bb8f750d42371a53ea8fb2a20ef1ad33d6a
Iva Pranjic <pranjic.iva213@gmail.com>
2021-12-07 11:12:29 +0100

    feature: create recipe detail page

---
+++ src/app/pages/RecipeDetail/RecipeDetail.tsx
@@ -0,0 +1,13 @@
+import { CssBaseline } from '@mui/material';
+import React from 'react';
+import Header from '../../components/Headers/HeaderRecipeDetail';
+
+export default (): JSX.Element => {
+  return (
+    <>
+      <CssBaseline />
+
+      <Header />
+    </>
+  );
+};
