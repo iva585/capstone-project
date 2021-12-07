1f4d6bd947f56783e8c0fe1318b349c6317411a4
Iva Pranjic <pranjic.iva213@gmail.com>
2021-12-07 15:27:41 +0100

    feature: create img header

--- src/app/components/Headers/HeaderRecipeDetail.tsx
+++ src/app/components/Headers/HeaderRecipeDetail.tsx
@@ -1,6 +1,14 @@
-import { AppBar } from '@mui/material';
+import { Box } from '@mui/system';
 import React from 'react';
+import './index.css';
 
 export default (): JSX.Element => {
-  return <AppBar></AppBar>;
+  return (
+    <Box sx={{ width: '100%', height: '200px', overflow: 'hidden' }}>
+      <img
+        className="image-header"
+        src="https://images.unsplash.com/photo-1616299908398-9af1134ad522?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=737&q=80"
+      />
+    </Box>
+  );
 };
