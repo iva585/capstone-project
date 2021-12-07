3835347df26b68dcc0c5e367e8e3e9922df44513
Iva Pranjic <pranjic.iva213@gmail.com>
2021-12-07 16:27:50 +0100

    feature: add image bar with star button

--- src/app/components/Headers/HeaderRecipeDetail.tsx
+++ src/app/components/Headers/HeaderRecipeDetail.tsx
@@ -1,5 +1,12 @@
-import { Card, CardContent, Typography } from '@mui/material';
+import {
+  Card,
+  CardContent,
+  IconButton,
+  ImageListItemBar,
+  Typography,
+} from '@mui/material';
 import { Box } from '@mui/system';
+import StarBorderRoundedIcon from '@mui/icons-material/StarBorderRounded';
 import React from 'react';
 import './index.css';
 
@@ -16,6 +23,20 @@ export default (): JSX.Element => {
           className="image-header"
           src="https://images.unsplash.com/photo-1616299908398-9af1134ad522?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=737&q=80"
           alt="pesto pasta"
+        />
+        <ImageListItemBar
+          sx={{
+            background:
+              'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, ' +
+              'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+          }}
+          position="top"
+          actionIcon={
+            <IconButton sx={{ color: 'white' }} aria-label="tasty pesto pasta">
+              <StarBorderRoundedIcon />
+            </IconButton>
+          }
+          actionPosition="right"
         />
       </Box>
       <Card
