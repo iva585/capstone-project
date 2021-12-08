c704878ae31a520cd2eeb1a7a495afd9521254eb
Iva Pranjic <pranjic.iva213@gmail.com>
2021-12-08 10:44:10 +0100

    feature: create story for header recipe detail

---
+++ src/app/components/Headers/headerRecipe.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import HeaderRecipeDetail from './HeaderRecipeDetail';
+
+export default {
+  title: 'Component/HeaderRecipeDetail',
+  component: HeaderRecipeDetail,
+};
+
+export const HeaderRecipe = (): JSX.Element => <HeaderRecipeDetail />;
