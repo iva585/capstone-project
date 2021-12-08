aa17592e12d5d94e29688799e631db789c3893b1
Iva Pranjic <pranjic.iva213@gmail.com>
2021-12-08 18:43:50 +0100

    fix: add padding bottom to main

--- src/app/pages/RecipeDetail/RecipeDetail.tsx
+++ src/app/pages/RecipeDetail/RecipeDetail.tsx
@@ -6,13 +6,14 @@ import IngredientsList from '../../components/IngredientsList';
 import Footer from '../../components/Footer';
 import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
 import DoneOutlinedIcon from '@mui/icons-material/DoneOutlined';
+import './index.css';
 
 export default (): JSX.Element => {
   const [editing, setEditing] = useState<boolean>(false);
   return (
     <>
       <CssBaseline />
-      <main>
+      <main className="bottom-padding">
         <Header />
         <IngredientsList />
         <Divider variant="middle" />
