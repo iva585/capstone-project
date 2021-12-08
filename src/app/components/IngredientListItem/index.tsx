b5f4afb8a3be6c2be5291ed222d5f554cd4d9a34
Iva Pranjic <pranjic.iva213@gmail.com>
2021-12-08 15:06:17 +0100

    feature: add to shopping list on add button click

--- src/app/components/IngredientListItem/index.tsx
+++ src/app/components/IngredientListItem/index.tsx
@@ -7,6 +7,8 @@ import {
   Typography,
 } from '@mui/material';
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { addShoppingListItem } from '../../reducers/shoppingListReducer';
 
 type IngredientItemType = {
   id: number;
@@ -19,13 +21,22 @@ type Props = {
 };
 
 export default (props: Props): JSX.Element => {
+  const dispatch = useDispatch();
+  const handleAddToShoppingList = () => {
+    dispatch(addShoppingListItem(props.item.title));
+    alert('Added to Shopping List!');
+  };
   return (
     <ListItem>
       <Typography>{props.item.amount}</Typography>
       <Divider sx={{ height: 28, m: 1 }} orientation="vertical" />
       <ListItemText>{props.item.title}</ListItemText>
 
-      <IconButton edge="end" aria-label="add to shopping list">
+      <IconButton
+        onClick={handleAddToShoppingList}
+        edge="end"
+        aria-label="add to shopping list"
+      >
         <Add />
       </IconButton>
     </ListItem>
