398b5533ceb560747cf3e6c8fbb127be7b615cd6
Iva Pranjic <pranjic.iva213@gmail.com>
2021-12-09 11:40:20 +0100

    feature: style tag text

--- src/app/components/Tag/Tag.tsx
+++ src/app/components/Tag/Tag.tsx
@@ -11,7 +11,9 @@ type Props = { tag: TagType };
 export default (props: Props): JSX.Element => (
   <Card>
     <ListItem>
-      <Typography>{props.tag.title}</Typography>
+      <Typography sx={{ fontWeight: 'light', fontSize: 'default' }}>
+        {props.tag.title}
+      </Typography>
     </ListItem>
   </Card>
 );
