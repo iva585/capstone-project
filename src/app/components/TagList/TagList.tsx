1ddd1cd7a3b104619e1621c02b23f3f76f98aad5
Iva Pranjic <pranjic.iva213@gmail.com>
2021-12-09 11:36:28 +0100

    feature: style TagList

--- src/app/components/TagList/TagList.tsx
+++ src/app/components/TagList/TagList.tsx
@@ -6,11 +6,25 @@ const initialState = [
   { id: 1, title: 'dinner' },
   { id: 2, title: 'pasta' },
   { id: 3, title: 'italian' },
+  { id: 4, title: 'dinner' },
+  { id: 5, title: 'pasta' },
+  { id: 6, title: 'italian' },
+  { id: 7, title: 'pasta' },
+  { id: 8, title: 'italian' },
 ];
 
 export default (): JSX.Element => {
   return (
-    <List sx={{ mt: '10px' }}>
+    <List
+      sx={{
+        mt: '10px',
+        p: '15px',
+        display: 'flex',
+        flexWrap: 'wrap',
+        rowGap: '3px',
+        columnGap: '7px',
+      }}
+    >
       {initialState.map((tag) => (
         <Tag key={tag.id} tag={tag} />
       ))}
