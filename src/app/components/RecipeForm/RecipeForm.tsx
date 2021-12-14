7bee76a78b22a027e8a3a12c2365435f83cf6335
Iva Pranjic <pranjic.iva213@gmail.com>
2021-12-14 11:34:12 +0100

    add placeholder image for future file upload

--- src/app/components/RecipeForm/RecipeForm.tsx
+++ src/app/components/RecipeForm/RecipeForm.tsx
@@ -1,27 +1,64 @@
-import { Card, CardContent, Input } from '@mui/material';
+import {
+  Box,
+  Card,
+  CardContent,
+  IconButton,
+  ImageListItemBar,
+  Input,
+} from '@mui/material';
 import React from 'react';
+import StarBorderRoundedIcon from '@mui/icons-material/StarBorderRounded';
 
 export default (): JSX.Element => {
   return (
-    <Card
+    <Box
       sx={{
-        alignSelf: 'center',
-        width: '70%',
-        p: 0,
-        borderRadius: '20px',
+        display: 'flex',
+        flexDirection: 'column',
       }}
     >
-      <CardContent
+      <Box sx={{ width: '100%', height: '200px', overflow: 'hidden' }}>
+        <img
+          className="image-header"
+          src="https://images.unsplash.com/photo-1616299908398-9af1134ad522?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=737&q=80"
+          alt="pesto pasta"
+        />
+        <ImageListItemBar
+          sx={{
+            background:
+              'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, ' +
+              'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+          }}
+          position="top"
+          actionIcon={
+            <IconButton sx={{ color: 'white' }}>
+              <StarBorderRoundedIcon />
+            </IconButton>
+          }
+          actionPosition="right"
+        />
+      </Box>
+      <Card
         sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
+          mt: -7,
+          alignSelf: 'center',
+          width: '70%',
+          p: 0,
+          borderRadius: '20px',
         }}
       >
-        <Input placeholder="Add Recipe Title Here" />
+        <CardContent
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Input placeholder="Add Recipe Title Here" />
 
-        <Input placeholder="Add Description Here" />
-      </CardContent>
-    </Card>
+          <Input placeholder="Add Description Here" />
+        </CardContent>
+      </Card>
+    </Box>
   );
 };
