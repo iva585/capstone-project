5aaca51dd450a9698a8d624c48f39f945239cd4b
Iva Pranjic <pranjic.iva213@gmail.com>
2021-12-14 17:09:34 +0100

    feature: add list title

--- src/app/components/RecipesList/RecipesList.tsx
+++ src/app/components/RecipesList/RecipesList.tsx
@@ -1,10 +1,20 @@
-import { List, ListItem } from '@mui/material';
+import { List, ListItem, Typography } from '@mui/material';
 import React from 'react';
 import CardDefault from '../RecipeCards/CardDefault';
 
 export default (): JSX.Element => {
   return (
     <List sx={{ display: 'flex', flexDirection: 'column', p: '7px' }}>
+      <Typography
+        variant="h6"
+        sx={{
+          alignSelf: 'start',
+          pl: '16px',
+          textTransform: 'uppercase',
+        }}
+      >
+        All Recipes
+      </Typography>
       <ListItem>
         <CardDefault />
       </ListItem>
