56eed93f012948c7de786b218b5e6b396dcf9608
Iva Pranjic <pranjic.iva213@gmail.com>
2021-12-14 17:10:51 +0100

    fix: remove unneeded routes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -9,14 +9,8 @@ export default (): JSX.Element => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Recipes />} />
-    </Routes>
-    <Routes>
       <Route path="/shopping-list" element={<ShoppingList />} />
-    </Routes>
-    <Routes>
       <Route path="/inventory" element={<Inventory />} />
-    </Routes>
-    <Routes>
       <Route path="/1" element={<RecipeDetail />} />
     </Routes>
   </BrowserRouter>
