bc5568ade4a41335f39d45340c100b1ca7aaed7a
Iva Pranjic <pranjic.iva213@gmail.com>
2021-12-14 17:29:18 +0100

    add mock data for recipes list

--- src/app/components/RecipeCards/CardDefault/index.tsx
+++ src/app/components/RecipeCards/CardDefault/index.tsx
@@ -4,41 +4,44 @@ import {
   Card,
   CardActions,
   CardContent,
+  ListItem,
   Typography,
 } from '@mui/material';
 import React from 'react';
 
 export default (): JSX.Element => {
   return (
-    <Card>
-      <Box
-        sx={{
-          height: '200px',
-          overflow: 'hidden',
-        }}
-      >
-        <img
-          className="image-header"
-          src="https://images.unsplash.com/photo-1616299908398-9af1134ad522?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=737&q=80"
-          alt="pesto pasta"
-        />
-      </Box>
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="h2">
-          Something tasty
-        </Typography>
-        <Typography variant="body2" color="textSecondary" component="p">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ab
-          voluptates cumque delectus rerum quam unde fugiat facere corrupti
-          dolor magni.
-        </Typography>
-      </CardContent>
+    <ListItem>
+      <Card>
+        <Box
+          sx={{
+            height: '200px',
+            overflow: 'hidden',
+          }}
+        >
+          <img
+            className="image-header"
+            src="https://images.unsplash.com/photo-1616299908398-9af1134ad522?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=737&q=80"
+            alt="pesto pasta"
+          />
+        </Box>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Something tasty
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ab
+            voluptates cumque delectus rerum quam unde fugiat facere corrupti
+            dolor magni.
+          </Typography>
+        </CardContent>
 
-      <CardActions>
-        <Button size="small" color="primary">
-          View full recipe
-        </Button>
-      </CardActions>
-    </Card>
+        <CardActions>
+          <Button size="small" color="primary">
+            View full recipe
+          </Button>
+        </CardActions>
+      </Card>
+    </ListItem>
   );
 };
