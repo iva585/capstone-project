39986cf3df314b721e987cd06c46654ce76ee72e
Iva Pranjic <pranjic.iva213@gmail.com>
2021-12-15 01:20:46 +0100

    feature: add search bar

--- src/app/components/Headers/HeaderRecipes.tsx
+++ src/app/components/Headers/HeaderRecipes.tsx
@@ -1,15 +1,28 @@
-import { AppBar, InputBase } from '@mui/material';
+import { AppBar, InputBase, Paper } from '@mui/material';
 import React from 'react';
 import SearchIcon from '@mui/icons-material/Search';
 
 export default () => {
   return (
-    <AppBar className="header">
-      <InputBase
-        placeholder="Search…"
-        inputProps={{ 'aria-label': 'search' }}
-      />
-      <SearchIcon />
+    <AppBar className="header-flex">
+      <Paper
+        sx={{
+          display: 'flex',
+          width: '60%',
+          height: '2.2rem',
+          alignSelf: 'center',
+          verticalAlign: 'center',
+          mt: 1.5,
+          borderRadius: 5,
+        }}
+      >
+        <InputBase
+          sx={{ p: 2 }}
+          placeholder="Search for a recipe.."
+          inputProps={{ 'aria-label': 'search' }}
+        />
+        <SearchIcon sx={{ m: 1 }} />
+      </Paper>
     </AppBar>
   );
 };
