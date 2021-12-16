0bf75a35f3de92f38d74fdbccc9462a1f1689ab1
Iva Pranjic <pranjic.iva213@gmail.com>
2021-12-16 11:21:18 +0100

    feature: change color palette

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -4,12 +4,16 @@ import './globals.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './store';
+import { ThemeProvider } from '@mui/material';
+import theme from './theme';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </Provider>
+  </React.StrictMode>,
   document.querySelector('#app')
 );
---
+++ src/app/theme.ts
@@ -0,0 +1,11 @@
+import { lightGreen } from '@mui/material/colors';
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: lightGreen,
+  },
+});
+
+export default theme;
