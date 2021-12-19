72c211dcb943607625f018bdf8421368b28598be
Iva Pranjic <pranjic.iva213@gmail.com>
2021-12-19 16:33:28 +0100

    feature: render starred list from storage

--- src/app/components/StarredRecipesList/StarredRecipesList.tsx
+++ src/app/components/StarredRecipesList/StarredRecipesList.tsx
@@ -1,32 +1,11 @@
 import { List, Typography } from '@mui/material';
 import React from 'react';
+import { getAllRecipeItemsFromStorage } from '../../api/storage/recipes';
 import CardStarred from '../RecipeCards/CardStarred';
 
-const initialState = [
-  {
-    id: 1,
-    title: 'Pesto Pasta',
-    description: 'Best served with a glass of Sauvignon Blanc!',
-  },
-  {
-    id: 2,
-    title: 'Very Tasty Pasta',
-    description:
-      'Reprehenderit praesentium ipsa alias, sint, iure odit illo voluptate, quia cum distinctio qui adipisci',
-  },
-  {
-    id: 3,
-    title: 'Pesto Pasta',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus ut, nulla consectetur incidunt molestiae libero recusandae explicabo.',
-  },
-  {
-    id: 4,
-    title: 'Basil Pasta',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus ut, nulla consectetur incidunt molestiae libero recusandae explicabo.',
-  },
-];
+const recipes = getAllRecipeItemsFromStorage();
+
+const starredRecipes = [...recipes].filter((recipe) => recipe.starred === true);
 
 export default (): JSX.Element => {
   return (
@@ -43,7 +22,7 @@ export default (): JSX.Element => {
         Starred Recipes
       </Typography>
       <List sx={{ display: 'flex', p: '16px', overflow: 'scroll' }}>
-        {initialState.map((item) => (
+        {starredRecipes.map((item) => (
           <CardStarred key={item.id} item={item} />
         ))}
       </List>
