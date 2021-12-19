6345bae4fed00e56e20bd26fa77e9930a8173766
Iva Pranjic <pranjic.iva213@gmail.com>
2021-12-19 16:17:56 +0100

    feature: list recipes from storage

--- src/app/components/RecipesList/RecipesList.tsx
+++ src/app/components/RecipesList/RecipesList.tsx
@@ -1,33 +1,9 @@
 import { List, Typography } from '@mui/material';
 import React from 'react';
+import { getAllRecipeItemsFromStorage } from '../../api/storage/recipes';
 import CardDefault from '../RecipeCards/CardDefault';
 
-const initialState = [
-  {
-    id: 1,
-    title: 'Warm Couscous Salad',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed ducimus reiciendis unde.',
-  },
-  {
-    id: 2,
-    title: 'Very Tasty Pasta',
-    description:
-      'Reprehenderit praesentium ipsa alias, sint, iure odit illo voluptate, quia cum distinctio qui adipisci',
-  },
-  {
-    id: 3,
-    title: 'Pesto Pasta',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus ut, nulla consectetur incidunt molestiae libero recusandae explicabo.',
-  },
-  {
-    id: 4,
-    title: 'Basil Pasta',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus ut, nulla consectetur incidunt molestiae libero recusandae explicabo.',
-  },
-];
+const recipes = getAllRecipeItemsFromStorage();
 
 export default (): JSX.Element => {
   return (
@@ -42,7 +18,7 @@ export default (): JSX.Element => {
       >
         All Recipes
       </Typography>
-      {initialState.map((item) => (
+      {recipes.map((item) => (
         <CardDefault key={item.id} item={item} />
       ))}
     </List>
